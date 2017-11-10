318f902e7b6e5bbf0fac3f18390412a24ee3582b
pcunnell <philip.cunnell@clickfox.com>
2017-11-10 08:01:39 -0700

    learning all the types for ts

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Angular 4 app for practice for building websites.
 FAQ app will add and delete 'FAQ'-type blocks of comments with conditional rendering and local storage
 
--hopfully I'll use some sort of store / state mgmt
--no Testing for this app
+- Hopfully I'll use some sort of store / state mgmt
+- No Testing for this app
--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -6,26 +6,41 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent {
-  name = 'john dildo';
-  emoji;
-  person = {
-    firstName: 'steve',
-    lastName: 'johnson'
-  }
+  name: string = 'johnathan doe';
+  age: number = 3;
+  hasChildren: boolean = true;
+  city: any = 'New Jurzey';
+  myNumbersArray: number[] = [1,2,3,4];
+  myStringArray: string[] = ['hello', 'world', 'poop'];
+  myAnyArray: any[] = [1, 'hi', true];
+  myTuple: [string, number] = ['hello', 3];
+  unusable: void = undefined; // or null
+  u: undefined = undefined; // or null too 
+  n: null = null; // or undefined
 
   constructor() {
-    this.name = this.name.split('').reverse().join('');
-    this.emoji = '😡';
-    this.hasEmoji();
-    this.hasEmoji();
-    this.hasEmoji();
+    this.myStringArray = ['ello']
   }
-
-  hasEmoji() {
-    this.emoji += '😈';
-  }
-
-  showEmoji() {
-    return this.emoji;
-  }
+  // name = 'john dildo';
+  // emoji;
+  // person = {
+  //   firstName: 'steve',
+  //   lastName: 'johnson'
+  // }
+  //
+  // constructor() {
+  //   this.name = this.name.split('').reverse().join('');
+  //   this.emoji = '😡';
+  //   this.hasEmoji();
+  //   this.hasEmoji();
+  //   this.hasEmoji();
+  // }
+  //
+  // hasEmoji() {
+  //   this.emoji += '😈';
+  // }
+  //
+  // showEmoji() {
+  //   return this.emoji;
+  // }
 }
