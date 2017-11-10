4dc4dc73df62794cd70371a4048886c8ad68c50d
pcunnell <philip.cunnell@clickfox.com>
2017-11-10 08:10:37 -0700

    types with interfaces and using them as arrays

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Customer } from './Customer';
 
 @Component({
   selector: 'sandbox',
@@ -6,41 +7,32 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent {
-  name: string = 'johnathan doe';
-  age: number = 3;
-  hasChildren: boolean = true;
-  city: any = 'New Jurzey';
-  myNumbersArray: number[] = [1,2,3,4];
-  myStringArray: string[] = ['hello', 'world', 'poop'];
-  myAnyArray: any[] = [1, 'hi', true];
-  myTuple: [string, number] = ['hello', 3];
-  unusable: void = undefined; // or null
-  u: undefined = undefined; // or null too 
-  n: null = null; // or undefined
+  customer: Customer
+  customers: Customer[];
 
   constructor() {
-    this.myStringArray = ['ello']
+    this.customer = {
+      id: 1,
+      name: 'john',
+      email: 'string@stirng.com'
+    }
+
+    this.customers = [
+      {
+        id: 1,
+        name: 'john',
+        email: 'string@stirng.com'
+      },
+      {
+        id: 2,
+        name: 'phil',
+        email: 'phil@stirng.com'
+      },
+      {
+        id: 3,
+        name: 'ryan',
+        email: 'ryan@stirng.com'
+      }
+    ]
   }
-  // name = 'john dildo';
-  // emoji;
-  // person = {
-  //   firstName: 'steve',
-  //   lastName: 'johnson'
-  // }
-  //
-  // constructor() {
-  //   this.name = this.name.split('').reverse().join('');
-  //   this.emoji = '😡';
-  //   this.hasEmoji();
-  //   this.hasEmoji();
-  //   this.hasEmoji();
-  // }
-  //
-  // hasEmoji() {
-  //   this.emoji += '😈';
-  // }
-  //
-  // showEmoji() {
-  //   return this.emoji;
-  // }
 }
