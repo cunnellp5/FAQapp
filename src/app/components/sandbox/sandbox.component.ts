3d259023bcd8e5ab1fe4829979c643b4142e47f2
pcunnell <philip.cunnell@clickfox.com>
2017-11-13 06:49:58 -0700

    conditional rendering with ngswitch ngif and ng templates

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -7,28 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent {
-  people = ['Rick', 'Daryl', 'Carl', 'Glen'];
-
-  people2 = [
-    {
-      firstName: 'Rick',
-      lastName: 'Sanchez'
-    },
-    {
-      firstName: 'daryl',
-      lastName: 'dixon'
-    },
-    {
-      firstName: 'carl',
-      lastName: 'grimes'
-    },
-    {
-      firstName: 'poopy',
-      lastName: 'butthole'
-    },
-  ]
-
-  constructor() {
-    this.people[2] = 'KARolley'
-  }
+  name: string = 'john dildoe';
+  showName: boolean = true;
+  greeting: number = 1;
 }
